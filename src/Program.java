c7d2ae53fd9a1892a18d0f59ae6131a0ca563554
Jens Henrik Skuldbol <jenshenrik.skuldbol@gmail.com>
2013-09-15 16:11:29 +0200

    - Added input file. - Redid file I/O code.

--- src/Program.java
+++ src/Program.java
@@ -7,6 +7,10 @@ import java.io.IOException;
 import java.io.FileOutputStream;
 import java.io.File;
 import java.io.BufferedOutputStream;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 import confidentiality.Confidentiality;
 
@@ -20,28 +24,35 @@ public class Program {
 	
 	public static void main(String args[]) {
 		// Check for parameters
-		if (args.length != 3) {
-			System.out.println(
-					"Usage:\n\n$>java Enryption <action> <input> <output>\n\n" +
-					" -action:\t'e' for encrypt or 'd' for decrypt\n" +
-					" -input:\tpath to input file\n" +
-					" -output:\tpath to output file\n");
-			System.exit(0);
-		}
+//		if (args.length != 3) {
+//			System.out.println(
+//					"Usage:\n\n$>java Enryption <action> <input> <output>\n\n" +
+//					" -action:\t'e' for encrypt or 'd' for decrypt\n" +
+//					" -input:\tpath to input file\n" +
+//					" -output:\tpath to output file\n");
+//			//System.exit(0);
+//			args[0] = "e";
+//			args[1] = "input";
+//			args[2] = "output";
+//		}
 
 		Program p = new Program();
 		
-		p.action = args[0];
-		p.inputPath = args[1];
-		p.outputPath = args[2];
+		p.action = "e"; //args[0];
+		p.inputPath = "input"; //args[1];
+		p.outputPath = "output"; //args[2];
 	
 		p.run();
 	}
 
 	public void run() {
-		key = getInput("Please enter symmetric key:").getBytes();
-		byte[] data = readFile(inputPath);
-		writeFile(data);
+		//key = getInput("Please enter symmetric key:").getBytes();
+		try {
+			byte[] data = readFile(inputPath);
+			writeFile(data);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public String getInput(String msg) {
@@ -50,46 +61,11 @@ public class Program {
 		return in.next();
 	}
 
-	public byte[] readFile(String path) {
-		InputStream is = null;
-		try {
-			is = new FileInputStream(path);
-		} catch (FileNotFoundException e) {
-			System.out.println(e);
-			System.exit(1);
-		}
-
-		int nRead;
-		byte[] data = new byte[1024];
-		
-		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-		
-		try {
-			while ((nRead = is.read(data, 0, data.length)) != -1) {
-				System.out.println("Read " + data);
-				buffer.write(data, 0, nRead);
-			}
-		
-			buffer.flush();
-		} catch (IOException e) {
-			System.out.println(e);
-			System.exit(1);
-		}
-		
-		return buffer.toByteArray();
+	public byte[] readFile(String path) throws IOException {
+		return Files.readAllBytes(Paths.get(path));
 	}
 
-	public void writeFile(byte[] data) {
-		try {
-			FileOutputStream fos = new FileOutputStream(new File(outputPath));
-			BufferedOutputStream bos = new BufferedOutputStream(fos);
-			bos.write(data);
-		} catch (FileNotFoundException e) {
-			System.out.println(e);
-			System.exit(1);
-		} catch (IOException e) {
-			System.out.println(e);
-			System.exit(1);
-		}
+	public void writeFile(byte[] data) throws IOException {
+		Files.write(Paths.get(outputPath), data, StandardOpenOption.WRITE);
 	}
 }
