25f9ce2cfd94d3f5dd7381ca885c8de44e2f40c0
Jens Henrik Skuldbol <jenshenrik.skuldbol@gmail.com>
2013-09-17 20:59:58 +0200

    - Added BC Provider.

--- src/confidentiality/Confidentiality.java
+++ src/confidentiality/Confidentiality.java
@@ -1,23 +1,28 @@
 package confidentiality;
 
-import java.security.Security;
-
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.Security;
 
 public class Confidentiality {
 
 	Cipher c;
 	SecretKeySpec factory;
+	IvParameterSpec ivSpec;
 	
 	public Confidentiality(byte[] key) {
 		try {
+			Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+			
+			
+			ivSpec = new IvParameterSpec("12345768".getBytes());
 			c = Cipher.getInstance("AES");
 			factory = new SecretKeySpec(key, "AES");
 		} catch (NoSuchAlgorithmException e) {
